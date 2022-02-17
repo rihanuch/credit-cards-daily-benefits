3747bfdf3c7ae4e50ea9555911b22a7a82c6881d
Rodrigo Hanuch <rihanuch@uc.cl>
2022-02-17 00:52:24 -0300

    feat: initial santander scrapper

---
+++ scrappers/cl/santander/index.js
@@ -0,0 +1,42 @@
+const puppeteer = require("puppeteer");
+const generators = require("../../generators/format.js");
+
+module.exports.scrapperSantander = async function () {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("https://banco.santander.cl/beneficios?segmento=s-personas");
+
+  const data = await page.$$eval(".content-up", (nodes) => {
+    return nodes.map((node) => {
+      let children = Array.from(node.children);
+      children = children.filter(
+        (child) => child.tagName === "H2" || child.tagName === "P"
+      );
+      return {
+        company: children[0].textContent.trim(),
+        benefit: children[1].textContent.trim(),
+      };
+    });
+  });
+  await browser.close();
+
+  const result = [...new Set(data.map((el) => el.company))].map((company) =>
+    generators.companyBenefitGenerator({ company: company })
+  );
+
+  for (const company of result) {
+    const name = company.company;
+    const originalLines = data
+      .filter((el) => el.company === name)
+      .map((el) => el.benefit);
+
+    company.originalLines = originalLines;
+    company.benefits = originalLines
+      .map((el) => generators.generateBenefitsFromLine(el))
+      .flat()
+      .map((el) => generators.benefitGenerator(el));
+  }
+
+  return result;
+};
