557fdce8b3a67d2873e3210df8c11c15be1e24d7
Rodrigo Hanuch <rihanuch@uc.cl>
2022-02-21 11:50:54 -0300

    feat: streamline responses

--- telegram/index.js
+++ telegram/index.js
@@ -2,41 +2,38 @@ require("dotenv").config();
 
 const scrapperSantander = require("../scrappers/cl/santander/index");
 const formatter = require("../scrappers/generators/text");
+const filterers = require("../scrappers/generators/filters");
 const TelegramBot = require("node-telegram-bot-api");
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
-async function responseMessage() {
-  const response = await scrapperSantander.scrapperSantander();
-  return formatter.benefitsFormatter(response);
+async function responseMessage({ data = null, filters = [] }) {
+  let response = data ? data : await scrapperSantander.scrapper();
+  responses = filterers.benefitsFilters(response, filters);
+  const messages = formatter.benefitsFormatter(response);
+  return messages;
 }
 
 // Matches "/echo [whatever]"
-bot.onText(/\/update/, async (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+bot.onText(/\/get (?<bank>\w+)(?<filter>.*)/, async (msg, match) => {
+  // unload the matches form the regex match
+  const {
+    groups: { bank, filter },
+  } = match;
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  const parsedFilters = filterers.parseFilters(filter);
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(process.env.TELEGRAM_CHANNEL, await responseMessage(), {
-    parse_mode: "Markdown",
+  // const chatId = msg.chat.id;
+  // const resp = match[1]; // the captured "whatever"
+  const messages = await responseMessage({
+    // data: jsonData(),
+    filters: parsedFilters,
   });
-});
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-// bot.on("message", async (msg) => {
-//   const chatId = msg.chat.id;
-//   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(
-//     parseInt(process.env.TELEGRAM_CHANNEL),
-//     await responseMessage(),
-//     {
-//       parse_mode: "Markdown",
-//     }
-//   );
-// });
+  for (const message of messages) {
+    bot.sendMessage(process.env.TELEGRAM_CHANNEL, message, {
+      parse_mode: "Markdown",
+    });
+  }
+});
