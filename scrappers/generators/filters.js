9f564dc258077ae686c2f1c2a66c444646563f11
Rodrigo Hanuch <rihanuch@uc.cl>
2022-02-20 22:37:11 -0300

    feat: added filters for benefits

---
+++ scrappers/generators/filters.js
@@ -0,0 +1,24 @@
+module.exports.parseFilters = function (filterText) {
+  filterText = filterText.toLowerCase().trim();
+  if (filterText === "all" || filterText === "") {
+    return [];
+  }
+  return filterText.split(",").map((text) => text.trim());
+};
+
+module.exports.benefitsFilters = function (benefits, filters = []) {
+  // we remove all the non specified benefits type
+  benefits.forEach((company) => {
+    // if no specification is given, then we return all the benefits
+    if (filters === []) {
+      return;
+    }
+
+    // filter by specified and allowed filter type
+    company.benefits = company.benefits.filter((benefit) =>
+      filters.includes(benefit.type)
+    );
+  });
+  // return non empty companies
+  return benefits.filter((company) => company.benefits.length !== 0);
+};
