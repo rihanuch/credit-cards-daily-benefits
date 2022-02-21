4c7255954610050cc94bd50ff750daf08701748c
Rodrigo Hanuch <rihanuch@uc.cl>
2022-02-21 10:23:47 -0300

    feat: added parsers

--- scrappers/generators/text.js
+++ scrappers/generators/text.js
@@ -1,14 +1,37 @@
+const MAXIMUM_MSG_LEN = 4096;
+
 module.exports.benefitsFormatter = function (benefits) {
   benefits.sort((a, b) => a.company.localeCompare(b.company));
 
-  text = "";
+  const payload = [];
+  let currentText = "";
   benefits.forEach((company) => {
-    text += `*${company.company}*`;
-    text += company.benefits.reduce(
-      (prev, next) => prev + `\n - ${next.value}`, // (${next.type})`,
-      ""
-    );
-    text += "\n\n";
+    if (company.benefits.length === 0) {
+      return;
+    }
+    const text = companyToString(company);
+    currentText = splitIntoMultiple(currentText, text, payload);
   });
-  return text.slice(0, 4096);
+  payload.push(currentText);
+  return payload;
 };
+
+function companyToString(company) {
+  let text = `*${company.company}*`;
+  text += company.benefits.reduce(
+    (prev, next) => prev + `\n - ${next.value}`,
+    ""
+  );
+  text += "\n\n";
+  return text;
+}
+
+function splitIntoMultiple(currentText, text, payload) {
+  if (currentText.length + text.length > MAXIMUM_MSG_LEN) {
+    payload.push(currentText);
+    currentText = text;
+  } else {
+    currentText += text;
+  }
+  return currentText;
+}
