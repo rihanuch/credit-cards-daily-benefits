f7b987f4a09caf037f565a71651575495c83b67e
Rodrigo Hanuch <rihanuch@uc.cl>
2022-02-21 14:22:13 -0300

    refactor: use redis instead of files

--- scrappers/generators/files.js
+++ scrappers/generators/files.js
@@ -1,15 +1,17 @@
-const fs = require("fs");
+const KEY_EXPIRATION_TIME_SECONDS = 60 * 60 * 24;
 
-module.exports.loadOrScrape = async function (path, method) {
-  try {
-    if (fs.existsSync(path)) {
-      return JSON.parse(fs.readFileSync(path));
-    } else {
-      const data = await method();
-      await fs.writeFileSync(path, JSON.stringify(data));
-      return data;
-    }
-  } catch (err) {
-    console.error(err);
+module.exports.loadOrScrape = async function (bank, method, client) {
+  const exists = await client.exists(bank);
+  if (exists) {
+    console.log(`key: '${bank}' exists: fetching from redis`);
+    const data = JSON.parse(await client.get(bank));
+    return data;
+  } else {
+    console.log(`key: '${bank}' does not exists: fetching from url`);
+    const data = await method();
+    await client.set(bank, JSON.stringify(data), {
+      EX: KEY_EXPIRATION_TIME_SECONDS,
+    });
+    return data;
   }
 };
