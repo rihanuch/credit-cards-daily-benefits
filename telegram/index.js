c2e425e9e48c5ede3522e85b237ecacef4b89124
Rodrigo Hanuch <rihanuch@uc.cl>
2022-02-21 14:22:50 -0300

    feat: use redis

--- telegram/index.js
+++ telegram/index.js
@@ -5,13 +5,15 @@ const formatter = require("../scrappers/generators/text");
 const filterers = require("../scrappers/generators/filters");
 const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
+const redis = require("redis");
 const app = express();
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+const client = redis.createClient();
 
 async function responseMessage({ data = null, filters = [] }) {
-  let response = data ? data : await scrapperSantander.scrapper();
+  let response = data ? data : await scrapperSantander.scrapper(client);
   responses = filterers.benefitsFilters(response, filters);
   const messages = formatter.benefitsFormatter(response);
   return messages;
@@ -43,6 +45,11 @@ bot.onText(/\/get (?<bank>\w+)(?<filter>.*)/, async (msg, match) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function () {
+app.listen(PORT, async function () {
+  await client.connect({
+    url: process.env.REDIS_URL
+      ? process.env.REDIS_URL
+      : "redis://localhost:6379",
+  });
   console.log(`Server is running at port ${PORT}`);
 });
